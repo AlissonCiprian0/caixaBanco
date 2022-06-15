31b4c9d8a828f5c4e95af05d64e7510b6bb346af
AlissonCiprian0 <alisson.cipriano@outlook.com>
2022-06-15 13:22:15 -0300

    Created options menu

--- src/CaixaBanco.java
+++ src/CaixaBanco.java
@@ -1,34 +1,77 @@
-public class CaixaBanco {
-    private String usuarioLogado;
-    private Boolean logado = false;
-    private Double saldo;
-    private String cpf;
-    private String senha;
+import java.util.ArrayList;
 
-    public Boolean verificaLogin(String cpf, String nomeDeUsuario) {
+public class CaixaBanco {
+    private Integer usuarioLogado = -1;
+    private Boolean logado = false;
+
+    private ArrayList <Usuario> usuarios = new ArrayList <Usuario>();
+
+    public void setUsuarios() {
+        usuarios.add(new Usuario(00.00, "487.891.108-54", "123", "Alisson"));
+    }
+
+    public void registra(Usuario usuario) {
+        usuarios.add(usuario);
+        System.out.printf("Usuário %s registrado!\n\n", usuario.getNome());
+    }
+
+    public Integer verificaLogin(String cpf) {
         if (logado) {
-            if (this.cpf == cpf) {
-                return true;
-            }
-            else {
-                logado=true;
-                this.cpf = cpf;
-                this.senha = senha;
-                this.usuarioLogado=nomeDeUsuario;
-                return true;
+            logado = false;
+            this.usuarioLogado = -1;
+        }
+
+        Integer i = 0;
+        Boolean userExist = false;
+
+        for (i = 0; i < usuarios.size(); i++) {
+            if (cpf.equals(usuarios.get(i).getCpf())) {
+                userExist = true;
+                break;
             }
         }
 
-        logado=true;
-        this.cpf = cpf;
-        this.senha = senha;
-        this.usuarioLogado=nomeDeUsuario;
-
-        return true;
+        if (userExist)
+            return i;
+        return -1;
     }
-    public void logar(String cpf, String senha, String nomeDeUsuario) {
-        verificaLogin(cpf, nomeDeUsuario);
-        System.out.printf("usuario logado: %s", this.usuarioLogado);
+    public void logar(String cpf, String senha) {
+        Integer userExist = verificaLogin(cpf);
 
+        if(userExist > -1) {
+            if (senha.equals(usuarios.get(userExist).getSenha())) {
+                logado = true;
+                this.usuarioLogado = userExist;
+
+                System.out.printf("Usuário logado: %s\n", usuarios.get(usuarioLogado).getNome());
+            }
+            else System.out.print("Senha incorreta.");
+        }
+        else {
+            System.out.print("Usuário não encontrado.");
+        }
+    }
+
+    public void depositar(Double valor) {
+        usuarios.get(usuarioLogado).setSaldo(usuarios.get(usuarioLogado).getSaldo() + valor);
+        System.out.println("Valor depositado!");
+    }
+
+    public void sacar(Double valor) {
+        if ((usuarios.get(usuarioLogado).getSaldo() - valor) < -600.00) {
+            System.out.println("\nSaque não permitido. Limite de crédito atingido.");
+        }
+        else {
+            usuarios.get(usuarioLogado).setSaldo(usuarios.get(usuarioLogado).getSaldo() - valor);
+            System.out.printf("Valor sacado!");
+        }
+    }
+
+    public void mostrarSaldo() {
+        System.out.printf("\nSaldo: R$ %.2f", usuarios.get(usuarioLogado).getSaldo());
+    }
+
+    public Integer getUsuarioLogado() {
+        return usuarioLogado;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,100 @@
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args) {
-        CaixaBanco caixa1 = new CaixaBanco();
-        Scanner  scan = new Scanner(System.in);
-        String nome = scan.nextLine();
-        caixa1.logar("111.222.333-11",  "22",  nome);
 
+    public static void clearScreen() {
+        System.out.println(new String(new char[50]).replace("\0", "\r\n"));
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Scanner scan = new Scanner(System.in);
+        CaixaBanco caixa1 = new CaixaBanco();
+        Integer escolha = 0;
+
+        caixa1.setUsuarios();
+
+        while (escolha != 6) {
+            System.out.print("\n\nCAIXA ELERÔNICO \n----------------------------------");
+            System.out.println("\nMenu");
+            System.out.println("1 - Logar");
+            System.out.println("2 - Registrar-se");
+            System.out.println("3 - Ver saldo");
+            System.out.println("4 - Sacar");
+            System.out.println("5 - Depositar");
+            System.out.println("6 - Sair");
+
+            escolha = scan.nextInt();
+            scan.nextLine();
+
+            if (escolha > 2 && escolha != 6 && caixa1.getUsuarioLogado() == -1) {
+                System.out.println("Faça login primeiro!");
+                TimeUnit.SECONDS.sleep(2);
+                clearScreen();
+            }
+            else {
+                switch (escolha) {
+                    case 1:
+                        clearScreen();
+                        System.out.println("Login");
+                        System.out.print("CPF: ");
+
+                        String cpf = scan.nextLine();
+                        System.out.print("Senha: ");
+
+                        String senha = scan.nextLine();
+
+                        clearScreen();
+                        caixa1.logar(cpf, senha);
+                        TimeUnit.SECONDS.sleep(2);
+                        break;
+                    case 2:
+                        clearScreen();
+                        System.out.println("Registro de usuário");
+
+                        System.out.print("Nome: ");
+                        String registraNome = scan.nextLine();
+
+                        System.out.print("CPF: ");
+                        String registraCpf = scan.nextLine();
+
+                        System.out.print("Senha: ");
+                        String registraSenha = scan.nextLine();
+
+                        clearScreen();
+                        caixa1.registra(new Usuario(00.00, registraCpf, registraSenha, registraNome));
+                        TimeUnit.SECONDS.sleep(2);
+                        break;
+                    case 3:
+                        clearScreen();
+                        caixa1.mostrarSaldo();
+                        TimeUnit.SECONDS.sleep(2);
+                        break;
+                    case 4:
+                        clearScreen();
+                        System.out.println("Saque");
+
+                        System.out.println("Quanto você quer sacar?");
+                        Double valorSaque = scan.nextDouble();
+                        caixa1.sacar(valorSaque);
+                        TimeUnit.SECONDS.sleep(2);
+                        break;
+                    case 5:
+                        clearScreen();
+                        System.out.println("Depósito");
+
+                        System.out.println("Quanto você quer depositar?");
+                        Double valorDeposito = scan.nextDouble();
+                        caixa1.depositar(valorDeposito);
+                        TimeUnit.SECONDS.sleep(2);
+                        break;
+                    case 6:
+                        clearScreen();
+                        System.out.println("Até a próxima!");
+                        TimeUnit.SECONDS.sleep(2);
+                        break;
+                }
+            }
+        }
     }
 }
